6d5b498c8e4ab2fb1f50dcaa64d0b0ed47f8f5de
Hunter Ware <hunter.ware@gmail.com>
2017-11-24 10:56:33 -0800

    update setup instructions for el7

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+########## OSX
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -5,8 +6,22 @@
 brew install htop ssh-copy-id xtitle mosh
 brew install vim --with-override-system-vi --with-python3 --with-luajit
 brew install colordiff ripgrep vim cscope neovim sift fzf xz stow global telnet highlight mas the_silver_searcher
+sudo pip3 install neovim
+
+########## EL7
+# EPEL
+sudo yum install epel-release
+# emacs repo
+URL=https://copr.fedorainfracloud.org/coprs/outman/emacs/repo/epel-7/outman-emacs-epel-7.repo
+curl -# -m 8 $URL -o /etc/yum.repos.d/emacs.repo
+yum update emacs-nox
+# Packages
+sudo yum install colordiff cscope neovim stow global highlight the_silver_searcher emacs-nox ctags
+sudo yum install python2-pip pythong34-devel python34-pip
+sudo pip3 install typing neovim
 
 # Setup neovim
+mkdir .vim
 ln -s ~/.vim ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 
@@ -18,6 +33,7 @@ rm installer.sh
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ./install.sh
+rm install.sh
 
 # Install powerlevel9k
 git clone https://github.com/hunterware/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
